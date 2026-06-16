fbb81a6afd2b62df83a6473341493f97acd2c053
adequateremedy <157491572+adequateremedy@users.noreply.github.com>
2026-06-16 18:31:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Divine Studio backend is running.");
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Divine Studio backend is running.");
+});
+
+server.listen(3000);
